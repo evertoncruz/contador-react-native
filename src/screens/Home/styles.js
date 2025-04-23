713b7b163a180608ce577bdf14cc266aa942afd1
Everton Cruz <cruz.everton@gmail.com>
2025-04-23 10:00:42 -0300

    created home screen

---
+++ src/screens/Home/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    heigth: "10px",
+  },
+  title: {
+    color: "#FFF",
+  },
+});
