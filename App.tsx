4eb0dab523e822f35bc538687bc566b4e8b76222
Everton Cruz <cruz.everton@gmail.com>
2025-04-23 11:54:30 -0300

    inserted view container

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { CounterProvider } from "./src/context/CounterContext";
 import HomeScreen from "./src/screens/HomeScreen/HomeScreen";
+import { StyleSheet, View } from "react-native";
 
 const App = () => (
   <CounterProvider>
-    <HomeScreen />
+    <View style={styles.container}>
+      <HomeScreen />
+    </View>
   </CounterProvider>
 );
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
